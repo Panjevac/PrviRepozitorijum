ef4fadbcf5e491a04e44d3c202ac5f3dd3d94f0d
Panjevac <92999190+Panjevac@users.noreply.github.com>
2021-10-29 11:20:17 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;    // ovo je za datatable - bez toga nemamo tu funkciju
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_9_prva
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable ucenik = new DataTable();
+            string conectionstring = "Data source= INF_4_04\\SQLPBG; initial catalog= MilosP49; Integrated security= true";                              // jednu kosu crtu menjamo sa dve - to je c ovski nacin (kod data source)
+        }
+    }
+}
